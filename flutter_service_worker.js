2bc941230a8982798caf325c08f0d3912a911cc3
Salah Alshafey <salahalshafey@gmail.com>
2024-05-01 22:36:30 +0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e77eb66e764204e844bc2014e003e61f",
-"/": "e77eb66e764204e844bc2014e003e61f",
+"index.html": "5a7441088b05365863dc429e4ce00ab3",
+"/": "5a7441088b05365863dc429e4ce00ab3",
 "main.dart.js": "5c6b86cfe33453e8cb1b6001b0dfff4c",
 "manifest.json": "356bbc44638406f5b2bf58a82522ecca",
 "version.json": "ace03002a7138d5497a75ea7d20cf4d2"};
